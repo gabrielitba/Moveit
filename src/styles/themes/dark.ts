a42b1ac57d7cef95d2db41ca8529c03b4cc0ab96
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-02-23 14:27:38 -0300

    feat: movendo arquivos do projeto em react

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,34 @@
+interface DarkTheme {
+  title: string;
+  colors: {
+    white: string;
+    background: string;
+    grayLine: string;
+    text: string;
+    textHighlight: string;
+    title: string;
+    red: string;
+    green: string;
+    blue: string;
+    blueDark: string;
+    blueTwitter: string;
+  };
+}
+
+export default {
+  title: 'dark',
+
+  colors: {
+    white: '#fff',
+    background: '#202020',
+    grayLine: '#dcdde0',
+    text: '#666666',
+    textHighlight: '#b3b9ff',
+    title: '#2e384d',
+    red: '#e83f5b',
+    green: '#4cd62b',
+    blue: '#5965e0',
+    blueDark: '#4953b8',
+    blueTwitter: '#2aa9e0',
+  },
+} as DarkTheme;
