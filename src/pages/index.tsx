db5227849a50022b49bbfa724b705d4a3bfd432d
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-03-01 09:53:20 -0300

    feat: teste GetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,17 @@ interface HomeProps {
   challengesCompleted: number;
 }
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
+
 const Home = (props: HomeProps) => {
   const { theme } = useTheme();
 
@@ -33,15 +44,3 @@ const Home = (props: HomeProps) => {
 };
 
 export default Home;
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
-
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted),
-    },
-  };
-};
